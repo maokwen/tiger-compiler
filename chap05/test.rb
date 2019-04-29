7e2d193f034db3839eeb6dfed0d4174e774af27c
MaoKwen <18361388+MaoKwen@users.noreply.github.com>
2019-04-29 15:45:35 +0800

    test method changed to makefile

--- chap05/test.rb
+++
@@ -1,7 +0,0 @@
-system 'make'
-(1..49).each do |i|
-    testfile = "test#{i.to_s}.tig"
-    printf "\033[1;36;40m" + testfile + "\033[0m" + ': ' + "\033[0;33;40m\n"
-    system './a.out ../testcases/' + testfile
-    printf "\033[0m"
-end
