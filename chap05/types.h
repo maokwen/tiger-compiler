e37a63890d4483cfaa541e010a7d29a18ce81aba
maokwen <18361388+MaoKwen@users.noreply.github.com>
2019-05-06 00:33:42 +0800

    format

--- chap05/types.h
+++ chap05/types.h
@@ -10,13 +10,14 @@ typedef struct Ty_tyList_ *Ty_tyList;
 typedef struct Ty_field_ *Ty_field;
 typedef struct Ty_fieldList_ *Ty_fieldList;
 
-struct Ty_ty_ {enum {Ty_record, Ty_nil, Ty_int, Ty_string, Ty_array,
-		       Ty_name, Ty_void} kind;
-	       union {Ty_fieldList record;
-		      Ty_ty array;
-		      struct {S_symbol sym; Ty_ty ty;} name;
-		    } u;
-	     };
+struct Ty_ty_ {
+  enum { Ty_record, Ty_nil, Ty_int, Ty_string, Ty_array,
+         Ty_name, Ty_void} kind;
+  union { Ty_fieldList record;
+          Ty_ty array;
+          struct { S_symbol sym; Ty_ty ty;} name;
+  } u;
+};
 
 struct Ty_tyList_ {Ty_ty head; Ty_tyList tail;};
 struct Ty_field_ {S_symbol name; Ty_ty ty;};
